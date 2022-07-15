3b642873767e1f89a24eed5482c9e0952d877e6f
Brandon Davis <brandon.davis@conehealth.com>
2022-07-14 21:54:25 -0400

    function complete

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,7 @@
 
 const tmdbApiKey = "346f7b7cb4a8eacfd5f60caf07af955f";
 const rapidApiKey = "4de443414emsh4a4ea1571d88c69p17feeajsn6962f58e5c81";
+const moviePullLimit = 5;
 const providers = [
     {
         name: "netflix",
@@ -37,10 +38,25 @@ var getMovieId = function (id) {
   const apiUrl = "https://api.themoviedb.org/3/discover/movie?api_key="+tmdbApiKey+"&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&with_people="+id+"&with_watch_monetization_types=flatrate"
   console.log(apiUrl);
 
+  //clear movies in the event of the function being called again
+  movies = [];
+
   fetch(apiUrl).then(function (response) {
     if (response.ok) {
       response.json().then(function (data) {
         console.log(data);
+        //iterate through each element and set data // will need to solve for limits
+        data.results.forEach(function (element, index) {
+
+          var movieObj = {
+            title: element.original_title,
+            id: element.id,
+            poster: element.poster_path,
+            overview: element.overview,
+            streams: ""
+          }
+          movies.push(movieObj);
+        })
       })
     }
     else {
