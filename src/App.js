d1fa0839a02a631b77fe1c508ed8209ac26ce9be
Max Kottong <max.kottong@gmail.com>
2023-02-15 19:01:55 -0500

    File structure changes, initial react app, test component for react

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import logo from './logo.svg';
+import Test from "./components/testComponent";
+import './App.css';
+
+function App() {
+    return (
+        <Test />
+  );
+}
+
+export default App;
